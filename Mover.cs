ef46a0449145fb8f30178868da13b448350ca76f
beckynavas <55064512+beckynavas@users.noreply.github.com>
2019-11-10 10:14:02 -0500

    Add files via upload

---
+++ Mover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
